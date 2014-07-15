751337cff4121b29e0313444e0f571dc24aa2dc7
Andrey Petrov <19graff91@gmail.com>
2014-07-15 12:31:10 +0300

    Add simple controller and Core project.

---
+++ HappyStation/HappyStation.Core/Entities/DatabaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HappyStation.Core.Entities
+{
+    public class DatabaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ HappyStation/HappyStation.Core/Entities/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HappyStation.Core.Entities
+{
+    public class Event : DatabaseEntity
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+    }
+}
