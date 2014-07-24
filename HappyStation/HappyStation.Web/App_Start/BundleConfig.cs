e0c975df3681ecfd20eadc6e9fab8edab5c76ac5
Andrey Petrov <19graff91@gmail.com>
2014-07-24 16:13:34 +0300

    test

--- HappyStation/HappyStation.Web/App_Start/BundleConfig.cs
+++ HappyStation/HappyStation.Web/App_Start/BundleConfig.cs
@@ -1,25 +1,27 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace HappyStation.Web
 {
     public class BundleConfig
     {
-        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/jquery.unobtrusive*",
+                "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css",
-                "~/Content/style-noscript.css",
-                "~/Content/style-desctop.css",
-                "~/Content/style-1000px.css",
-                "~/Content/style-mobile.css"));
+            bundles.Add(new StyleBundle("~/bundles/site").Include(
+                "~/Content/skel-noscript.css",
+                "~/Content/style.css",
+                "~/Content/style-desktop.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/init").Include(
+                "~/Scripts/site/skel.min.js",
+                "~/Scripts/site/skel-panels.min.js",
+                "~/Scripts/site/init.js"));
         }
     }
 }
--- HappyStation/HappyStation.Web/Controllers/HomeController.cs
+++ HappyStation/HappyStation.Web/Controllers/HomeController.cs
@@ -1,12 +1,44 @@
 ﻿using System.Web.Mvc;
 
+using HappyStation.Core.DatabaseContext;
+using HappyStation.Core.Services;
+using HappyStation.Core.Services.Implementations;
+using HappyStation.Web.Resources;
+
 namespace HappyStation.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly NewsRepository newsRepository;
+        private readonly EventsRepository eventsRepository;
+
+        public HomeController()
+        {
+            var dataContext = new DatabaseContext();
+            newsRepository = new NewsRepository(dataContext);
+            eventsRepository = new EventsRepository(dataContext);
+        }
+
+        public HomeController(NewsRepository newsRepository, EventsRepository eventsRepository)
+        {
+            this.newsRepository = newsRepository;
+            this.eventsRepository = eventsRepository;
+
+            ViewBag.Title = Strings.MainPage;
+        }
+
+        [HttpGet]
         public ActionResult Index()
         {
+            ViewBag.News = newsRepository.GetBy();
+            ViewBag.Events = eventsRepository.GetBy();
+
             return View();
+        }
+
+        public ActionResult About()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
