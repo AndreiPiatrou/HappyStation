ff2f7130ae3893267c8cb3a752be23a344b262da
Andrey Petrov <19graff91@gmail.com>
2014-07-24 16:07:32 +0300

    test

--- HappyStation/HappyStation.Core/Entities/DatabaseEntity.cs
+++ HappyStation/HappyStation.Core/Entities/DatabaseEntity.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HappyStation.Core.Entities
 {
     public class DatabaseEntity
     {
+        protected DatabaseEntity()
+        {
+            CreatedAt = DateTime.Now;
+        }
+
+        [Key]
         public int Id { get; set; }
 
+        [Required]
         public DateTime CreatedAt { get; set; }
 
         public bool IsDeleted { get; set; }
--- HappyStation/HappyStation.Core/Entities/Event.cs
+++ HappyStation/HappyStation.Core/Entities/Event.cs
@@ -1,17 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HappyStation.Core.Entities
 {
     public class Event : DatabaseEntity
     {
+        [Required]
         public string Title { get; set; }
 
         public string Description { get; set; }
 
         public string Text { get; set; }
 
-        public DateTime StartTime { get; set; }
+        public DateTime? StartTime { get; set; }
 
-        public DateTime EndTime { get; set; }
+        public DateTime? EndTime { get; set; }
     }
 }
